dd77a86848ef3fcf0f4c36568a3ee54ee44edc5d
liuchang914 <3316356843@qq.com>
2025-06-30 09:24:12 +0800

    应收款搜索条件

---
+++ vue3-element-admin/src/api/User/user.api.ts
@@ -0,0 +1,12 @@
+import request from '@/utils/request';
+
+const UserAPI = {
+  GetUserPage() {
+    return request({
+      url: "/api/app/user-server/user-info-list",
+      method: "get",
+    });
+  },
+}
+
+export default UserAPI;
