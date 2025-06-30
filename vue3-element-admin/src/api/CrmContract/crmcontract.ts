f86501f8245fd30368007e287a7d8b63be842022
GoodLietime <3528016071@qq.com>
2025-06-30 16:13:47 +0800

    存档06301613

--- vue3-element-admin/src/api/CrmContract/crmcontract.ts
+++ vue3-element-admin/src/api/CrmContract/crmcontract.ts
@@ -5,7 +5,6 @@ const CRM_CONTRACT_BASE_URL = "/api/app/crm-contract";
 //查询合同列表
 const CrmContractAPI = {
   getInfo(data: any) {
-    console.log(data);
     return request({
       url: `${CRM_CONTRACT_BASE_URL}/show-crm-contract-list`,
       method: "get",
